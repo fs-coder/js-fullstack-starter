d87ccc609a2fff466957621666ed5619c12e387f
rmlzy <rmlzy@outlook.com>
2020-03-01 11:49:29 +0800

    feat: Support Jest

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,7 +1,19 @@
+"use strict";
+
+const path = require("path");
+
 module.exports = (app, defaultConfig /* , dev */) => {
   const babelLoader = defaultConfig.module.rules[0];
 
   babelLoader.use.options.babelrc = true;
 
-  return defaultConfig;
+  return {
+    ...defaultConfig,
+    resolve: {
+      extensions: [".json", ".js", ".jsx"],
+      alias: {
+        client: path.join(__dirname, "../client")
+      }
+    }
+  };
 };
